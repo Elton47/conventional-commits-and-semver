dc5278b725c5ab4e88c767496550bf0b12e6a08b
Elton Memishaj <eltoni.memishi@live.com>
2021-08-03 12:44:12 +0200

    chore(swagger-gen): add swagger gen package & config for rest api gen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ApiConfiguration, ApiConfigurationInterface } from 'src/app/core/api/api-configuration';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ApiConfiguration,
+      useValue: { rootUrl: 'root_api_url_here' } as ApiConfigurationInterface
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
